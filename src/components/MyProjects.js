4d6906e8332fe139d39ed3902f95fecd0f478557
Palash jaiswal <pjpallu55@gmail.com>
2022-07-26 12:47:41 +0530

    Code Modifies

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -17,7 +17,7 @@ export default function MyProjects() {
       <div class="row">
 
         <div class="col-md-6 col-lg-4 text-center mb-5">
-          <div class="card">
+          <div class="card mx-2">
             <div class="content">
               <div class="front">
                 <h3 class="card-head title">Text Changer</h3>
@@ -34,7 +34,7 @@ export default function MyProjects() {
           </div>
         </div>
         <div class="col-md-6 col-lg-4 text-center mb-5">
-          <div class="card">
+          <div class="card mx-2">
             <div class="content">
               <div class="front">
                 <h3 class=" card-head title">Wikipedia Text Analysis</h3>
@@ -51,7 +51,7 @@ export default function MyProjects() {
           </div>
         </div>
         <div class="col-md-6 col-lg-4 text-center mb-5">
-          <div class="card">
+          <div class="card mx-2">
             <div class="content">
               <div class="front">
                 <h3 class="card-head title">Tic Tac Toe</h3>
